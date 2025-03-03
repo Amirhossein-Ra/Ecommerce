9d5356a5ef9fcac3cda6604b8999859815517636
Amirhossein-Ra <000amir111rahzany@gmail.com>
2025-03-03 19:58:29 +0330

    Setting Clerk Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-3">
       <div className="w-[100%] flex justify-end">
+        <UserButton />
         <ThemeToggle />
       </div>
-      <div className="font-barlow">welcome</div>
-      <Button variant="destructive">Click Here</Button>
+      <h1>Home Page</h1>
     </div>
   );
 }
